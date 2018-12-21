354d4952b1a73760dcb3afbcb63eb616436eae68
Matt Stuhring <whereyouatmatt@gmail.com>
2018-12-21 14:57:23 -0800

    Working with props

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -4,46 +4,45 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/App.scss';
 
-const Header = () => {
+const Header = (props) => {
+  console.log(props);
   return (
     <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Player: 1</span>
+      <h1>{ props.title }</h1>
+      <span className="stats">Player: { props.totalPlayers }</span>
     </header>
   );
 }
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
-        Matt
+        { props.name }
       </span>
 
-      <Counter />
+      <Counter score={ props.score } />
     </div>
   );
 }
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
+      <span className="counter-score">{ props.score }</span>
       <button className="counter-action increment"> + </button>
     </div>
   );
 }
 
-
-
 class App extends Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header />
+        <Header title="Scoreboard" totalPlayers={1} />
 
-        <Player />
+        <Player name="Matt" score={35} />
       </div>
     );
   }
